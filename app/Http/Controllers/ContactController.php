4c1e6ab3f1377c0fd9e9ff89818372b6426d8c3c
Alx Barrón <alxbarron@gmail.com>
2020-08-15 23:24:57 -0500

    :sparkles: Import contacts into db

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends CsvHandlerController
 {
-    protected $fields = ['team_id', 'name', 'email', 'phone'];
+    protected $fields = ['team_id', 'name', 'email', 'phone', 'sticky_phone_number_id'];
 
     /**
      * Display a listing of the resource.
@@ -32,7 +32,7 @@ class ContactController extends CsvHandlerController
     public function uploadFile(Request $request)
     {
         try {
-            $request->validate(['csv-file' => 'file|mimes:csv,txt']);
+            $request->validate(['csv-file' => 'file|mimes:csv,txt|between:1,200']);
         } catch (\Exception $e) {
             $response = ['success' => false, 'message' => $e->getMessage()];
             return response()->json($response, Response::HTTP_BAD_REQUEST);
@@ -75,8 +75,32 @@ class ContactController extends CsvHandlerController
             return response()->json($response, Response::HTTP_BAD_REQUEST);
         }
         
-        $file_path = $request->csv_file;
-        $csv_content = $this->getFileContent($file_path);
+        $csv = $this->getFileContent( $request->csv_file );
+        if ( count($csv) < 2 ) 
+            return response()->json(['success' => false, 'message' => 'File not suitable for import'], Response::HTTP_BAD_REQUEST);
+
+        array_shift($csv); // Remove header
+        $csv_fields = collect($request->csv_field);
+        $custom_fields = collect($request->custom_attr);
+        
+        try {
+            foreach ($csv as $csv_line) {
+                $contact_data = $csv_fields->mapWithKeys(function ($item, $key) use ($csv_line) {
+                    return [$item => $csv_line[$key]];
+                });
+                $contact = Contact::create( $contact_data->toArray() );
+                
+                $custom_attributes = [];
+                foreach($custom_fields as $key => $value) {
+                    array_push($custom_attributes, ['key' => $value, 'value' => $csv_line[$key]]);
+                }
+
+                $contact->customAttributes()->createMany($custom_attributes);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
 
         return response()->json(['success' => true], 200);
     }
